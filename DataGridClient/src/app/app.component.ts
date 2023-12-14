21e5a427ea3ad48b65dc426a7ba02849c2aea41b
caglatunc <caglatunc03@gmail.com>
2023-12-14 16:37:57 +0300

    Adding Loading

--- DataGridClient/src/app/app.component.ts
+++ DataGridClient/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ButtonRenderComponent } from './button-render/button-render.component';
 
 
@@ -15,59 +15,74 @@ import { ButtonRenderComponent } from './button-render/button-render.component';
 })
 export class AppComponent {
 
-  rowData:any = [];
-  
+  rowData: any = [];
+
   colDefs: any[] = [
     {
       headerName: "#",
-      valueGetter: (params:any) => params.node.rowIndex + 1,
-      width:30,
+      valueGetter: (params: any) => params.node.rowIndex + 1,
+      width: 30,
       floatingFilter: false,
       sort: false,
     },
-      { field: "name" },
-      { field: "summary" },
-      { field: "author" },
-      {
-        field: "publishDate",
-        valueFormatter: (params: any) => {
-          return new Date(params.value).toLocaleDateString('tr-TR', { day: '2-digit', month: '2-digit', year: 'numeric' });
-        }
+    { field: "name" },
+    { field: "summary" },
+    { field: "author" },
+    {
+      field: "publishDate",
+      valueFormatter: (params: any) => {
+        return new Date(params.value).toLocaleDateString('tr-TR', { day: '2-digit', month: '2-digit', year: 'numeric' });
       }
-    ];
+    }
+  ];
 
-   autoSizeStrategy:any = {
+  gridOptions: any = {
+    overlayLoadingTemplate:
+      `<span class="ag-overlay-loading-center">Yükleniyor...</span>`,
+    overlayNoRowsTemplate:
+      `<span style="padding: 10px;">No data available to display</span>`,
+  };
+
+  autoSizeStrategy: any = {
     type: 'fitGridWidth',
     defaultWidth: 100
   };
 
-    defaultColDef: any = {
-      editable: this.checkAuthorization(),
-      filter: true,
-      floatingFilter: true,
-      onCellValueChanged: (params:any)=> this.update(params),
-    }
+  defaultColDef: any = {
+    editable: this.checkAuthorization(),
+    filter: true,
+    floatingFilter: true,
+    onCellValueChanged: (params: any) => this.update(params),
+  }
+  
   constructor(private http: HttpClient) { }
 
   onGridReady(params: any) {
-    this.getAll();
+    params.api.showLoadingOverlay();
+    this.getAll(params);
   }
 
-  checkAuthorization(){
+  checkAuthorization() {
     return true;
   }
 
-  getAll(){
-    this.http.get("https://localhost:7126/api/Values/GetAll").subscribe(res=>{
-      this.rowData = res;
-      })
+  getAll(params:any = null) {
+    this.http.get("https://localhost:7126/api/Values/GetAll")
+    .subscribe({
+      next:(res)=>{
+        this.rowData = res
+      },
+      error:(err:HttpErrorResponse)=>{
+        params?.api?.showNoRowsOverlay();
+      }
+    })
   }
 
-  update(params:any){
+  update(params: any) {
     console.log(params.data);
     this.http.post("https://localhost:7126/api/Values/Update", params.data)
-    .subscribe(res=>{
-      this.getAll();
-    })
+      .subscribe(res => {
+        this.getAll();
+      })
   }
 }
